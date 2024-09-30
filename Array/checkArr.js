4b49fe56a11603c1825a0f223d30d1dde24a65a2
Amit Chapde <amitchapde2@gmail.com>
2024-09-30 12:21:51 +0530

    added comments

--- Array/checkArr.js
+++ Array/checkArr.js
@@ -2,22 +2,23 @@
 var check = function(nums) {
     let n=nums.length;
     let count=0;
+    //count the number of breaks in the sorted array 
     for(let i=1;i<n;i++){
         if(nums[i-1]>nums[i]){
             count++;
         }
     }
-
+    //check for break between last and first element as we are considering circular array
     if(nums[n-1]>nums[0]){
         count++;
     }
-
+    // The array can be sorted and rotated if there are at most 1 break
     return count<=1;
 };
 
 const testArrays = [
     [3, 4, 5, 1, 2],    // True: it's a rotated sorted array
-    [2, 1, 3, 4],       // False: more than one break
+    [2, 1, 3, 4],       // False: more than one break (2 > 1 and 4 > 3)
     [1, 2, 3, 4],       // True: already sorted
     [2, 3, 4, 5, 1],    // True: rotated sorted array
     [1],                // True: single element is considered sorted
