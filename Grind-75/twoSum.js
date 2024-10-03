34ca60e42ed92d52c3500c2651c71410a71d392b
Amit Chapde <amitchapde2@gmail.com>
2024-10-03 11:37:54 +0530

    added solution for twoSum for grind75

---
+++ Grind-75/twoSum.js
@@ -0,0 +1,14 @@
+let twoSum = function(nums, target) {
+    let map=new Map();
+    for(let i=0;i<nums.length;i++){
+        let num1=nums[i];
+        let num2=target-nums[i];
+        if(map.has(num2)){
+            return [i,map.get(num2)];
+        }
+        map.set(num1,i)
+    }
+};
+
+let nums=[3,2,4];
+console.log(twoSum(nums,6));
